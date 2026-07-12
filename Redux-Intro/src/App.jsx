04475dfdea08aa28326cb617bb112c3884fced59
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-07-12 12:42:24 -0700

    Fix main.js

--- Redux-Intro/src/App.jsx
+++ Redux-Intro/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/features/counterSlice';
 
 // Provide the Redux Store to React
 const App = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch();
   return (
     <div>
-      <h1>0</h1>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <h1>{count}</h1>
+      <button onClick={() => {
+        dispatch(increment())
+      }}>Increment</button>
+      <button onClick={() => {
+        dispatch(decrement())
+      }}>Decrement</button>
     </div>
   )
 }
