1d95c570a81cc81586a6dc60c241ddd1af0e6bc7
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-07-14 12:35:07 -0700

    Project Intro

---
+++ Redux-Project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full font-mono bg-cyan-500 text-3xl font-bold underline'>
+      Application
+    </div>
+  )
+}
+
+export default App
