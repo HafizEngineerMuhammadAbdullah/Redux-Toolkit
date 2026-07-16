81bbefafa2a8d925e48dc7784aeeb59a8152c044
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-07-16 12:09:09 -0700

    Dealing with API

--- Redux-Project/src/App.jsx
+++ Redux-Project/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { fetchUnsplashImages } from './api/mediaApi'
 
 const App = () => {
   return (
     <div className='h-screen w-full font-mono bg-cyan-500 text-3xl font-bold underline'>
       Application
+      <button className="py-2 px-4 border-2 border-e-red-950 rounded-xl cursor-pointer hover:bg-amber-300 hover:scale-105" onClick={async () => {
+        const data = await fetchUnsplashImages('cats', 1, 10);
+        console.log(data);
+      }
+      }>Get Photos</button>
     </div>
   )
 }
---
+++ Redux-Project/src/api/mediaApi.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_API_KEY;
+
+
+// function fetchUnsplashImages(query, page = 1, perPage = 10) {
+//   const url = `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=${perPage}`;
+//   return axios.get(url, {
+//     headers: {
+//       Authorization: `Client-ID ${UNSPLASH_KEY}`,
+//     },
+//   });
+// }
+
+async function fetchUnsplashImages(query, page = 1, perPage = 10) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+        params: {
+            query,
+            page,
+            perPage
+        },
+        headers: {
+            Authorization: `Client-ID ${UNSPLASH_KEY}`,
+        },
+    });
+    return response.data;
+
+}
+
+// function fetchPexelsVideos(query, page = 1, perPage = 10) {
+//     const url = `https://api.pexels.com/v1/videos/search?query=${query}&page=${page}&per_page=${perPage}`;
+//     return axios.get(url, {
+//         headers: {
+//             Authorization: PEXELS_KEY,
+//         },
+//     });
+// }
+
+
+
+async function fetchPexelsVideos(query, page = 1, perPage = 10) {
+    const response = await axios.get("https://api.pexels.com/v1/videos/search", {
+        params: {
+            query,
+            page,
+            perPage
+        },
+        headers: {
+            Authorization: `Client-ID ${UNSPLASH_KEY}`,
+        },
+    });
+    return response.data;
+}
+
+export { fetchUnsplashImages, fetchPexelsImages };
